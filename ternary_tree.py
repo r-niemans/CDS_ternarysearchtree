582764e0d287b3e9332ab6940ca8da00e44bdb88
Sophie Yam <sophie.Yam@student.uhasselt.be>
2025-05-01 14:50:40 +0200

    Improve ternary search tree by switching to character nodes
    Change how words are stored in the tree by breaking them into individual letters instead of storing whole words at once. Add exact
    search to distinguish prefix searches from full word searches.

--- ternary_tree.py
+++ ternary_tree.py
@@ -1,50 +1,63 @@
 class TtreeNode:
 
-    def __init__(self, string):
-        self._string = string
-        self._lt, self._gt = None, None
+    def __init__(self, character=None):
+        self._character = character
+        self._lt = None
+        self._gt = None
         self._equal = None
         self.is_end = False
 
-    def _insert(self, string):
-        if string == self._string:
+    def _insert(self, word, index=0):
+        if index == len(word):
             self.is_end = True
             return
-        if string < self._string:
+        char = word[index]      # per character and not full string
+        if self._character is None:
+            self._character = char
+        if char < self._character:
             if self._lt is None:
-                self._lt = TtreeNode(string)
-            else:
-                self._lt._insert(string)
-        elif string > self._string:
+                self._lt = TtreeNode(char)
+            self._lt._insert(word, index)
+        elif char > self._character:
             if self._gt is None:
-                self._gt = TtreeNode(string)
-            else:
-                self._gt._insert(string)
+                self._gt = TtreeNode(char)
+            self._gt._insert(word, index)
         else:
             if self._equal is None:
-                self._equal = TtreeNode(string[1:])
-            else:
-                self._equal._insert(string[1:])
+                self._equal = TtreeNode()
+            self._equal._insert(word, index + 1)
 
-    def _search(self, string):
-        if not string:
+    def _search(self, word, index=0, exact=False):  # exact parameter added
+        if word == '' and not exact:
+            return True
+        if index == len(word):
+            return self.is_end if exact else True
+        char = word[index]
+        if self._character is None:
             return False
-        if string == self._string:
-            return self.is_end
-        if string < self._string:
-            return self._lt is not None and self._lt._search(string)
+        if char < self._character:
+            return self._lt is not None and self._lt._search(word, index, exact)
+        elif char > self._character:
+            return self._gt is not None and self._gt._search(word, index, exact)
         else:
-            return self._gt is not None and self._gt._search(string)
+            return self._equal is not None and self._equal._search(word, index + 1, exact)
+
+
+    def _collect_words(self, results, prefix):
+        if self.is_end:
+            results.append(prefix)
+        if self._lt:
+            self._lt._collect_words(results, prefix)
+        if self._equal:
+            self._equal._collect_words(results, prefix + self._character)
+        if self._gt:
+            self._gt._collect_words(results, prefix)
 
     def _all_strings(self):
-        strings = [self._string]
-        if self._lt is not None:
-            strings.extend(self._lt._all_strings())
-        if self._gt is not None:
-            strings.extend(self._gt._all_strings())
-        if self._equal is not None:
-            strings.extend(self._equal._all_strings())
-        return strings
+        results = []
+        self._collect_words(results, "")
+        return results
+
 
     def __len__(self):
         length = 1
@@ -56,49 +69,48 @@ class TtreeNode:
             length += len(self._gt)
         return length
 
-    def _to_string(self, indent=''):
-        repr_str = indent + repr(self)
-        if self._lt is not None:
-            repr_str += '\n' + self._lt._to_string(indent + '  ')
-        if self._gt is not None:
-            repr_str += '\n' + self._gt._to_string(indent + '  ')
+    def _to_string(self, prefix=''):
+        repr_str = prefix + repr(self)
+        if self._lt:
+            repr_str += '\n' + self._lt._to_string(prefix + '  ')
+        if self._equal:
+            repr_str += '\n' + self._equal._to_string(prefix + '  ')
+        if self._gt:
+            repr_str += '\n' + self._gt._to_string(prefix + '  ')
         return repr_str
 
     def __repr__(self):
-        return self._string
+        return f"Node({self._character}, boolean={self.is_end})"
 
 
-class Ttree:
+class TernarySearchTree:
 
     def __init__(self):
-        self._root = None
+        self._root = TtreeNode()
+        self._size = 0
 
-    def insert(self, string):
-        if self._root is None:
-            self._root = TtreeNode(string)
-        else:
-            self._root._insert(string)
+    def insert(self, word):
+        if not self.search(word, exact=True):
+            self._size += 1
+        self._root._insert(word)
 
-    def search(self, string):
+
+    def search(self, word, exact=False):
         if self._root is None:
             return False
         else:
-            return self._root._search(string)
+            return self._root._search(word, exact=exact)
+
         
     def all_strings(self):
-        if self._root is None:
-            return []
-        else:
-            return self._root._all_strings()
+        return self._root._all_strings()
         
     def __len__(self):
-        if self._root is None:
-            return 0
-        else:
-            return len(self._root)
+        return self._size
+
     
     def __repr__(self):
         if self._root is None:
             return 'empty tree'
         else:
-            return self._root._to_string('')
+            return self._root._to_string()
