e437962a5b92996d841e318026b9ee8f2e39d820
Sophie Yam <sophie.Yam@student.uhasselt.be>
2025-05-01 13:10:56 +0200

    Introduce ternary search tree structure
    Modify the node definition to include an equal key, allowing three children instead of two.

--- ternary_tree.py
+++ ternary_tree.py
@@ -1,43 +1,57 @@
-class BtreeNode:
-    
+class TtreeNode:
+
     def __init__(self, string):
         self._string = string
         self._lt, self._gt = None, None
+        self._equal = None
+        self.is_end = False
 
     def _insert(self, string):
         if string == self._string:
+            self.is_end = True
             return
         if string < self._string:
             if self._lt is None:
-                self._lt = BtreeNode(string)
+                self._lt = TtreeNode(string)
             else:
                 self._lt._insert(string)
         elif string > self._string:
             if self._gt is None:
-                self._gt = BtreeNode(string)
+                self._gt = TtreeNode(string)
             else:
                 self._gt._insert(string)
-    
+        else:
+            if self._equal is None:
+                self._equal = TtreeNode(string[1:])
+            else:
+                self._equal._insert(string[1:])
+
     def _search(self, string):
+        if not string:
+            return False
         if string == self._string:
-            return True
-        elif string < self._string:
+            return self.is_end
+        if string < self._string:
             return self._lt is not None and self._lt._search(string)
         else:
             return self._gt is not None and self._gt._search(string)
-    
+
     def _all_strings(self):
         strings = [self._string]
         if self._lt is not None:
             strings.extend(self._lt._all_strings())
         if self._gt is not None:
             strings.extend(self._gt._all_strings())
+        if self._equal is not None:
+            strings.extend(self._equal._all_strings())
         return strings
 
     def __len__(self):
         length = 1
         if self._lt is not None:
             length += len(self._lt)
+        if self._equal is not None:
+            length += len(self._equal)
         if self._gt is not None:
             length += len(self._gt)
         return length
@@ -52,15 +66,16 @@ class BtreeNode:
 
     def __repr__(self):
         return self._string
-    
-class Btree:
-    
+
+
+class Ttree:
+
     def __init__(self):
         self._root = None
-        
+
     def insert(self, string):
         if self._root is None:
-            self._root = BtreeNode(string)
+            self._root = TtreeNode(string)
         else:
             self._root._insert(string)
 
