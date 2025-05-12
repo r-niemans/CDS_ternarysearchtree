81658405c7a67cbf612d09af86f04c6b6ec0a0c5
syam3500 <sophie.yam@student.uhasselt.be>
2025-05-12 10:20:46 +0200

    Create a new version of the ternary tree
    Add a new version of the ternary tree to conduct benchmarking.

---
+++ ternary_tree_B.py
@@ -0,0 +1,13 @@
+""""This is a ternary search tree implemented
+using object-oriented programming."""
+
+
+class TtreeNode:
+    """This object is a node in a ternary search tree (TST)."""
+
+    pass
+
+class TernarySearchTreeB:
+    """Ternary Search tree search for storing words"""
+
+    pass
