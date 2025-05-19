2743ba9389f1af551b226ad98c8f183ffce6ba90
r-niemans <rafke.niemans@gmail.com>
2025-05-19 19:52:38 +0200

    removed arrow files, forced main reset.

---
+++ vsc_py_file.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+
+import argparse
+
+arg_parser= argparse.ArgumentParser(description='Load dataset and implement Ternary Tree')
+arg_parser.add_argument('dataset', type=str, help='Path to dataset')
+arg_parser.add_argument('ternary_tree', type=str, help='Tree structure')
+args = arg_parser.parse_args()
+
+print(f'Dataset: {args.dataset}')
+print(f'Ternary Tree Type: {args.ternary_tree}')
+
