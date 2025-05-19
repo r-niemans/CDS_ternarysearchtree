be613b299ffc10372fc6ac39d5751580ba8c78d1
r-niemans <rafke.niemans@gmail.com>
2025-05-19 19:56:28 +0200

    file with another tree structure to run on HPC, with all functions in one .py file

--- vsc_py_file.py
+++ vsc_py_file.py
@@ -2,6 +2,8 @@
 
 import argparse
 
+# yet to add time and memory comparison in this file
+
 arg_parser= argparse.ArgumentParser(description='Load dataset and implement Ternary Tree')
 arg_parser.add_argument('dataset', type=str, help='Path to dataset')
 arg_parser.add_argument('ternary_tree', type=str, help='Tree structure')
