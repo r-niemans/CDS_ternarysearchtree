8b112f33e1be441b5f36124b8cd029e260ecbd83
Sophie Yam <sophie.Yam@student.uhasselt.be>
2025-05-20 16:31:24 +0200

    Add scripts
    Add Python scripts for benchmarking.

---
+++ HPC/insert_test.py
@@ -0,0 +1,12 @@
+import re
+from ternary_tree_minimalistic import TernarySearchTreeSparse
+import random
+
+with open("frequency_words.txt") as file:
+    words = [line.strip() for line in file]
+
+insert_sample = random.sample(words, k = 1000000)
+
+tst = TernarySearchTreeSparse()
+for word in insert_sample:
+    tst.insert(word)
---
+++ HPC/search_test.py
@@ -0,0 +1,14 @@
+from ternary_tree_minimalistic import TernarySearchTreeSparse
+import random
+
+with open("frequency_words.txt") as file:
+    words = [line.strip() for line in file]
+insert_sample = random.sample(words, k=1000000)
+
+tst = TernarySearchTreeSparse()
+for word in insert_sample:
+    tst.insert(word)
+
+insert_sample = random.sample(words, k=1000000)
+for word in insert_sample:
+    tst.search(word, exact=False)
